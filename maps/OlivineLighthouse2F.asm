1907eee59aaeae4277d96cd400049acd24033b31
nickjwilde <nickjwilde@gmail.com>
2022-04-25 00:07:34 -0700

    Fix merge conflicts

--- maps/OlivineLighthouse2F.asm
+++ maps/OlivineLighthouse2F.asm
@@ -57,25 +57,15 @@ UnknownScript_0x5afb3:
 UnknownScript_0x5afc7:
 	scall UnknownScript_0x5b06b
 	winlosstext SailorHuey1BeatenText, 0
-	readmem wHueyFightCount
-	ifequal 3, .Fight3
-	ifequal 2, .Fight2
-	ifequal 1, .Fight1
-	ifequal 0, .LoadFight0
-.Fight3:
 	checkevent EVENT_RESTORED_POWER_TO_KANTO
 	iftrue .LoadFight3
-.Fight2:
 	checkevent EVENT_BEAT_ELITE_FOUR
 	iftrue .LoadFight2
-.Fight1:
 	checkevent EVENT_CLEARED_RADIO_TOWER
 	iftrue .LoadFight1
-.LoadFight0:
 	loadtrainer SAILOR, HUEY1
 	startbattle
 	reloadmapafterbattle
-	loadmem wHueyFightCount, 1
 	clearflag ENGINE_HUEY_READY_FOR_REMATCH
 	end
 
@@ -83,7 +73,6 @@ UnknownScript_0x5afc7:
 	loadtrainer SAILOR, HUEY2
 	startbattle
 	reloadmapafterbattle
-	loadmem wHueyFightCount, 2
 	clearflag ENGINE_HUEY_READY_FOR_REMATCH
 	end
 
@@ -91,7 +80,6 @@ UnknownScript_0x5afc7:
 	loadtrainer SAILOR, HUEY3
 	startbattle
 	reloadmapafterbattle
-	loadmem wHueyFightCount, 3
 	clearflag ENGINE_HUEY_READY_FOR_REMATCH
 	end
 
